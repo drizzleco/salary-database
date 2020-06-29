c8c5f6d3252b1040c1747600dbc6f20da157555b
petabite <jmslca123@gmail.com>
2020-06-29 14:44:33 -0700

    document functions

--- static/js/main.js
+++ static/js/main.js
@@ -1,20 +1,32 @@
+/**
+ * Toggle clear button on search field appropriately on input
+ */
 $('.has-clear input[type="text"]').on('input propertychange', function () {
     var $this = $(this);
     var visible = Boolean($this.val());
     $this.siblings('.form-control-clear').toggleClass('d-none', !visible);
 }).trigger('propertychange');
 
+/**
+ * Click handler for clear button on search fields that clears the field
+ */
 $('.form-control-clear').click(function () {
     $(this).siblings('input[type="text"]').val('')
         .trigger('propertychange').focus();
 });
 
+/**
+ * Sort salaries table by the date column
+ */
 function dateSorter(a, b) {
     if (new Date(a) < new Date(b)) return 1;
     if (new Date(a) > new Date(b)) return -1;
     return 0;
 }
 
+/**
+ * Format employer and job title column with value as a link
+ */
 function linkFormatter(value, row, index, field) {
     if (field === 'EMPLOYER_NAME')
         return "<a href=\"?employer=" + value + "\">" + value + "</a>"
@@ -28,6 +40,9 @@ var formatter = new Intl.NumberFormat('en-US', {
     minimumFractionDigits: 0,
 });
 
+/**
+ * Format Number as a USD currency string w/o decimals (123456.0 -> $123,456)
+ */
 function salaryFormatter(value) {
     return formatter.format(value);
 }
