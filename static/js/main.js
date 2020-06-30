cad9d0ceb7653731ef9c21a61e007f856380196d
尸廾工L工尸 乙 <jmslca123@gmail.com>
2020-06-30 13:30:25 -0700

    Add Frontend Views (#3)
    * add flask views
    
    * use form values dict
    
    * document functions
    
    * change h3 to h2
    
    * tweak frontend view

---
+++ static/js/main.js
@@ -0,0 +1,52 @@
+/**
+ * Toggle clear button on search field appropriately on input
+ */
+$('.has-clear input[type="text"]').on('input propertychange', function () {
+    var $this = $(this);
+    var visible = Boolean($this.val());
+    $this.siblings('.form-control-clear').toggleClass('d-none', !visible);
+}).trigger('propertychange');
+
+/**
+ * Click handler for clear button on search fields that clears the field
+ */
+$('.form-control-clear').click(function () {
+    $(this).siblings('input[type="text"]').val('')
+        .trigger('propertychange').focus();
+});
+
+/**
+ * Sort salaries table by the date column
+ */
+function dateSorter(a, b) {
+    if (new Date(a) < new Date(b)) return 1;
+    if (new Date(a) > new Date(b)) return -1;
+    return 0;
+}
+
+/**
+ * Format employer and job title column with value as a link
+ */
+function linkFormatter(value, row, index, field) {
+    if (field === 'EMPLOYER_NAME')
+        return "<a href=\"?employer=" + value + "\">" + value + "</a>"
+    if (field === 'JOB_TITLE')
+        return "<a href=\"?title=" + value + "\">" + value + "</a>"
+    if (field === 'EMPLOYER_CITY')
+        return "<a href=\"?city=" + value + "\">" + value + "</a>"
+    if (field === 'EMPLOYER_STATE')
+        return "<a href=\"?state=" + value + "\">" + value + "</a>"
+}
+
+var formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 0,
+});
+
+/**
+ * Format Number as a USD currency string w/o decimals (123456.0 -> $123,456)
+ */
+function salaryFormatter(value) {
+    return formatter.format(value);
+}
