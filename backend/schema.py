4c76d5d72dd256a7bd3a72d77be020afa99cff36
petabite <jmslca123@gmail.com>
2020-07-02 13:09:06 -0700

    implement sort by year with graphql

--- backend/schema.py
+++ backend/schema.py
@@ -18,18 +18,21 @@ class Query(graphene.ObjectType):
         title=graphene.String(),
         city=graphene.String(),
         state=graphene.String(),
+        year=graphene.String(),
     )
 
     def resolve_salaries(self, info, **args):
         fuzzy_query = {}
         for query in ["employer", "title", "city", "state"]:
             fuzzy_query[query] = "%{}%".format(args.get(query, ""))
+        year = args.get("year")
+        year = year[-2:] if year else "%%"  # get year without century
         matched = Salary.get_query(info).filter(
             db.or_(SalaryModel.EMPLOYER_NAME.like(fuzzy_query["employer"])),
             db.or_(SalaryModel.JOB_TITLE.like(fuzzy_query["title"])),
             db.or_(SalaryModel.EMPLOYER_CITY.like(fuzzy_query["city"])),
             db.or_(SalaryModel.EMPLOYER_STATE.like(fuzzy_query["state"])),
-            # db.or_(Salary.year == int(session.get("year"))),  # this doesn't work
+            db.or_(db.func.substr(SalaryModel.EMPLOYMENT_START_DATE, -2).like(year)),
         )
         return matched.all()
 
