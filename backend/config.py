b2cc65a7c19aa11a1bdd206fa45c47e1e5fa28a4
Philip Zhang <jmslca123@gmail.com>
2020-07-20 18:51:47 -0700

    add local db url to config

--- backend/config.py
+++ backend/config.py
@@ -3,6 +3,8 @@ import os
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = (
+        os.getenv("DATABASE_URL") or "sqlite:///../data/salary.sqlite"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = "".join([chr(random.randint(65, 92)) for _ in range(50)])
